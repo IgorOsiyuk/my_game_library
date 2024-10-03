5aa9ae0bf5324c8638fb419f6a64adf0fb5c4eea
Ihar Asiyuk <rigidum.by@gmail.com>
2024-10-03 21:11:58 +0300

    db connecttion

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.getOrThrow<number>('PORT');
+
+  await app.listen(PORT);
 }
 bootstrap();
