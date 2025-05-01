29794e05210c8844b05363dbb7d901e8c6ba77f3
Ihar Asiyuk <rigidum.by@gmail.com>
2025-05-01 13:19:44 +0300

    validate file

--- apps/api/src/reviews/reviews.controller.ts
+++ apps/api/src/reviews/reviews.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Post, Request, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  ParseFilePipeBuilder,
+  Post,
+  Request,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { AuthGuard } from '../guards';
 
 import { CreateReviewDto } from './dto';
 import { ReviewsService } from './reviews.service';
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB в байтах
 
 @Controller('reviews')
 export class ReviewsController {
@@ -19,7 +31,17 @@ export class ReviewsController {
   @Post()
   @UseGuards(AuthGuard)
   @UseInterceptors(FileInterceptor('img'))
-  create(@Request() req: Request, @Body() createReviewDto: CreateReviewDto, @UploadedFile() img?: Express.Multer.File) {
+  create(
+    @Request() req: Request,
+    @Body() createReviewDto: CreateReviewDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addMaxSizeValidator({ maxSize: MAX_FILE_SIZE })
+        .addFileTypeValidator({ fileType: 'image/*' })
+        .build(),
+    )
+    img?: Express.Multer.File,
+  ) {
     return this.reviewsService.create(req['userId'], createReviewDto, img);
   }
 }
